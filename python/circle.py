4f6595eb251c65451afc354c05282b217846d770
nafiuadegbite <youngguru23@gmail.com>
2020-03-24 18:16:58 +0100

    python task

---
+++ python/circle.py
@@ -0,0 +1,11 @@
+# Import math
+import math
+
+#User input of the radius
+radius = int(input("Enter the radius of the circle: "))
+
+#Area of a circle
+area_of_circle = (math.pi) * (radius ** 2) 
+
+# Print out the solution
+print(area_of_circle)
